393695aa1a0ca85c26b900a9ccace28538a47c08
mirandachang <miranda.chang@kellogg.northwestern.edu>
2020-05-19 18:19:17 +0000

    updated weather URL

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ get "/" do
   key = "YOUR-API-KEY-GOES-HERE" # replace this with your real OpenWeather API key
 
   # construct the URL to get the API data (https://openweathermap.org/api/one-call-api)
-  url = "https://api.openweathermap.org/data/2.5/onecall?lat=#{lat}&lon=#{long}&units=#{units}&appid=#{key}"
+  url = "https://api.openweathermap.org/data/2.5/onecall?lat=40.582305&lon=-80.170219&units=imperial&appid=e2287273035896a82b22c35ccc9b94ff"
 
   # make the call
   @forecast = HTTParty.get(url).parsed_response.to_hash
